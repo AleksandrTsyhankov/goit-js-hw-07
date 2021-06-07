883b771b5f43f0f85d7257eaf027f19aaeef5e16
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-06-07 23:32:59 +0300

    3 commit
    Внес правки в задание №6 для корректной работы

--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,15 @@ const validationStyles = document.querySelectorAll("#validation-input");
 const validInputValueLength = parseInt(input.dataset.length);
 
 function onInputChanges(event) {
-    const inputValueLength = event.currentTarget.value.length;
+    const inputValueLength = event.target.value.length;
+
     console.log(inputValueLength, validInputValueLength);
-    if (inputValueLength > 0 && inputValueLength !== validInputValueLength) {
-        return input.classList.add('invalid');
-    }
-    else if (inputValueLength === validInputValueLength) {
-        return input.classList.add('valid');
-    }
-        return input.classList.remove('valid', 'invalid');
+    if (inputValueLength === validInputValueLength) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+    } else if (inputValueLength === 0) {
+        input.classList.remove('valid', 'invalid');
+    } else {input.classList.add('invalid');}
 }
 
 input.addEventListener('change', onInputChanges);
